f47a416d8a246b9d8f95d4e3832348a14d517fb5
Emily Keen <emily.keen711@gmail.com>
2017-05-04 12:47:33 -0400

    heroku work

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 8080;
+var PORT = process.env.PORT || 8080;
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
@@ -18,4 +18,4 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_Controller.js");
 app.use("/", routes);
-app.listen(port);
+app.listen(PORT);
